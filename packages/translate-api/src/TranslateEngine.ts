c3408999c0c3d08960bd7b744072f0110d48c6d8
JianyingLi <lijy91@foxmail.com>
2019-12-22 16:58:00 +0800

    Update LookUpResult model

--- packages/translate-api/src/TranslateEngine.ts
+++ packages/translate-api/src/TranslateEngine.ts
@@ -1,29 +1,38 @@
 interface LookUpOptions {
-  format?: string;
   sourceLanguage?: string;
   targetLanguage?: string;
 }
 interface LookUpResult {
   engine: string;               // 字典引擎
   word: string;                 // 单词
-  definitions?: Array<string>;  // 定义（基本释义）
+  definitions?: Array<any>;     // 定义（基本释义）
   ukIpa?: string;               // 英语-英式音标
   ukPronunciationUrl?: string;  // 英语-英式发音链接
   usIpa?: string;               // 英语-美式音标
   usPronunciationUrl?: string;  // 英语-美式发音链接
-  tenses?: Array<string>;       // 时态
-  sentences?: Array<string>;    // 例句
+  tenses?: Array<any>;          // 时态
+  sentences?: Array<any>;       // 例句
+  // 翻译（当查词非单词时需要采用下面的字段）
+  sourceText?: string,          // 原始文本
+  sourceAudioUrl?: string,      // 原始音频链接
+  targetText?: string,          // 目标文本
+  targetAudioUrl?: string,      // 目标音频链接
+  // 原始数据（翻译引擎返回的数据）
   originData: any;
 }
 
 interface TranslateTextOptions {
-  format?: string;
   sourceLanguage?: string;
   targetLanguage?: string;
 }
 interface TranslateTextResult {
-  sourceText?: string;
-  targetText?: string;
+  sourceLanguage?: string;      // 原始语言
+  targetLanguage?: string;      // 目标语言
+  sourceText?: string,          // 原始文本
+  sourceAudioUrl?: string,      // 原始音频链接
+  targetText?: string,          // 目标文本
+  targetAudioUrl?: string,      // 目标音频链接
+  // 原始数据（翻译引擎返回的数据）
   originData: any;
 }
 
