08b0a3e93177f65ae0d7261bfba65511f04178f7
JianyingLi <lijy91@foxmail.com>
2019-12-23 22:56:53 +0800

    Create PosTag.ts

---
+++ packages/translate-api/src/PosTag.ts
@@ -0,0 +1,14 @@
+const PosTag = {
+  ADJ: '形容词',
+  ADV: '副词',
+  CONJ: '连词',
+  DET: '限定词',
+  MODAL: '动词',
+  NOUN: '名词',
+  PREP: '介词',
+  PRON: '代词',
+  VERB: '动词',
+  OTHER: '其他',
+};
+
+export default PosTag;
